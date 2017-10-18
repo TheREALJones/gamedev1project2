926ed192308696f5edaeb473071ce701b35d0785
eti-yen <emeraldwatch15@gmail.com>
2017-10-18 03:49:47 +0100

    Title screen update

--- js/title.js
+++ js/title.js
@@ -3,7 +3,7 @@ let titleState = function() {
 }
 
 titleState.prototype.preload = function() {
-	game.load.image("title", "assets/Toby Philpott manipulated.png");
+	game.load.image("title", "assets/TobyOpeningCard.png");
 	game.load.audio("titleTheme", "assets/music/puppetintro.wav");
 }
 
@@ -13,15 +13,14 @@ titleState.prototype.create = function() {
 	music.play();
  
 	let title = game.add.image(0, 0, "title");
-	title.scale.setTo(750/1025, 750/1025);
 	title.centerX = game.world.centerX;
 	title.centerY = game.world.centerY;
 	
-	let startButton = game.add.button(450, 40, "f",  titleState.buttonPressed);
+	let startButton = game.add.button(75, 40, "f",  titleState.buttonPressed);
 	
 	let rectangle = game.add.graphics();
 	startButton.addChild(rectangle);
-	rectangle.beginFill(0xFFFFFF);
+	rectangle.beginFill(0xDDDDDD);
 	rectangle.drawRect(0, 0, 100, 40);
 	rectangle.centerX = startButton.width/2;
 	rectangle.centerY = startButton.height/2;
@@ -31,31 +30,18 @@ titleState.prototype.create = function() {
 	startText.centerX = rectangle.width/2;
 	startText.centerY = rectangle.height/2;
 	
-	let debugButton = game.add.button(450, 90, "f",  titleState.debugPressed);
+	let debugButton = game.add.button(75, 90, "f",  titleState.debugPressed);
 	rectangle = game.add.graphics();
 	debugButton.addChild(rectangle);
-	rectangle.beginFill(0xEEEEEE);
+	rectangle.beginFill(0xDDDDDD);
 	rectangle.drawRect(0, 0, 100, 40);
 	rectangle.centerX = debugButton.width/2;
 	rectangle.centerY = debugButton.height/2;
 	
 	let debugText = game.add.text(0, 0, "Test");
 	rectangle.addChild(debugText);
-	startText.centerX = rectangle.width/2;
-	startText.centerY = rectangle.height/2;
-	
-	let dragButton = game.add.button(450, 140, "f",  titleState.dragPressed);
-	rectangle = game.add.graphics();
-	dragButton.addChild(rectangle);
-	rectangle.beginFill(0xEEEEEE);
-	rectangle.drawRect(0, 0, 100, 40);
-	rectangle.centerX = dragButton.width/2;
-	rectangle.centerY = dragButton.height/2;
-	
-	let dragText = game.add.text(0, 0, "Drag Test");
-	rectangle.addChild(dragText);
-	startText.centerX = rectangle.width/2;
-	startText.centerY = rectangle.height/2;
+	debugText.centerX = rectangle.width/2;
+	debugText.centerY = rectangle.height/2;
 }
 
 titleState.prototype.update = function() {

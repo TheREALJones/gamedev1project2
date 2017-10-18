974a26fe9c3f493595cabaf2271fb496452d20b8
Drdoomdragon <rocking_sushi@yahoo.com>
2017-10-18 05:05:35 -0400

    Everything is centered!

--- js/startscreen.js
+++ js/startscreen.js
@@ -2,8 +2,11 @@ let startState = function(){
 
 }
 startState.prototype.create = function() {
-	let warning = game.add.text(200, 200, "This game is a work of ficton\n\ndeveloped by 5 incredibly tired college students.\n\nAny relation to any puppeteers real or imaginary\n\nis completely on you bud.", {font:'40px Arial', align: "center", fill: '#FFFFFF'})
-	game.add.tween(warning).to( { alpha: 0 }, 8000, Phaser.Easing.Linear.None, true);
+	let warning = game.add.text(200, 200, "This game is a work of ficton\ndeveloped by 5 incredibly tired college students.\nAny relation to any puppeteers real or imaginary\nis completely on you bud.", {font:'40px Arial', align: "center", fill: '#FFFFFF'})
+	game.add.tween(warning).to( { alpha: 0 }, 8500, Phaser.Easing.Linear.None, true);
+
+	warning.centerX = game.world.centerX;
+	warning.centerY = game.world.centerY;
 }
 
 
